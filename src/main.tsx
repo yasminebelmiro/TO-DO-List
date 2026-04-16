6b9abecf7d49be70e81b8b0ebfda773ed5db64dd
Yasmine Belmiro <devyasminebelmiro@gmail.com>
2026-04-16 10:42:46 -0300

    build: reconfiguração e rebuild da aplicação, recebe tailwind e typescript

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
-import GlobalStyles from './globalStyles.jsx'
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <GlobalStyles />
+  
     <App />
   </StrictMode>,
 )
